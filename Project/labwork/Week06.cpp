22fbd041e681804e9bdfa047bdc3d8f8346fb0e5
HocedezIne <ine.hocedez@student.howest.be>
2024-05-31 16:45:32 +0200

    changed background color

--- Project/labwork/Week06.cpp
+++ Project/labwork/Week06.cpp
@@ -55,7 +55,7 @@ void VulkanBase::beginRenderPass(const GP2_CommandBuffer& cmdBuffer, VkFramebuff
 	renderPassInfo.renderArea.extent = extent;
 
 	std::array<VkClearValue,2> clearValues{};
-	clearValues[0].color = { {0.5f, 0.5f, 0.5f, 1.f} };
+	clearValues[0].color = { {0.f, 0.f, 0.f, 1.f} };
 	clearValues[1].depthStencil = { 1.f, 0 };
 	renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
 	renderPassInfo.pClearValues = clearValues.data();
